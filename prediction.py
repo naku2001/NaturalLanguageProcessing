58f194cfdcf673655133d1dad0963ae8189d5c1f
BLESSMORE2 <76495184+BLESSMORE2@users.noreply.github.com>
2023-09-26 11:11:00 +0200

    Update prediction.py
    commit

--- prediction.py
+++ prediction.py
@@ -1,4 +1,4 @@
-#Imports
+# Imports
 import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
@@ -6,10 +6,8 @@ import numpy as np
 import pickle
 
 # Load the model and tokenizer
-model = load_model('best_model2.h5',compile=False)
+model = load_model('best_model2.h5', compile=False)
 tokenizer = pickle.load(open('tokenizerr.pkl', 'rb'))
-
-
 
 
 def predict_next_words(model, tokenizer, text, num_words=1):
@@ -47,12 +45,7 @@ def predict_next_words(model, tokenizer, text, num_words=1):
     return ' '.join(text.split(' ')[-num_words:])
 
 
-
 def main():
-
-    
-    
-
     html_temp = """
     <body style="background-color:red;">
     <div style="background-color:green ;padding:10px">
@@ -66,19 +59,12 @@ def main():
     user_input = st.text_input("Enter any five words: ")
     lst = list(user_input.split())
 
-            
-
     if st.button("Generate"):
-        
-        if (user_input is not None and len(lst)==5):
-        
-            result =  predict_next_words(model, tokenizer, user_input, num_words=1)
+        if (user_input is not None and len(lst) == 5):
+            result = predict_next_words(model, tokenizer, user_input, num_words=1)
             st.success(result)
-        
         else:
             st.write("Please enter five words only")
-        
-        
 
 
 if __name__ == '__main__':
